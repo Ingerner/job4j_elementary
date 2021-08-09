8afbed52156e8ccc2ae312dd6699b5b9c6aaf3cd
Ingener <korotkiy.o@yandex.ru>
2021-08-09 23:02:28 +0300

    6.7.1. Моно строка в матрице. [#214126]

--- src/ru/job4j/array/Matrix.java
+++ src/ru/job4j/array/Matrix.java
@@ -4,7 +4,7 @@ public class Matrix {
     public static int[][] multiple(int size) {
         int[][] array = new int[size][size];
         for(int i=0; i<array.length; i++) {
-            for(int j=0; j<array[i].length; j++){
+            for(int j = 0; j < array[i].length; j++){
                 array[i][j] = (i + 1) * ( j + 1);
             }
         }
