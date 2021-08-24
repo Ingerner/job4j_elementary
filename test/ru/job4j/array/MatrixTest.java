935b2ec033b3705f19f409192c343730443f68bc
Oleg <korotkiy.o@yandex.ru>
2021-08-24 15:39:35 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#34741 #189281]

--- test/ru/job4j/array/MatrixTest.java
+++ test/ru/job4j/array/MatrixTest.java
@@ -17,18 +17,4 @@ public class MatrixTest {
         };
         Assert.assertArrayEquals(expected, result);
     }
-
-    @Test
-    public void when5on5() {
-        int size = 5;
-        int[][] result = Matrix.multiple(size);
-        int[][] expected = {
-                {1, 2, 3, 4, 5},
-                {2, 4, 6, 8, 10},
-                {3, 6, 9, 12, 15},
-                {4, 8, 12, 16, 20},
-                {5, 10, 15, 20, 25}
-        };
-        Assert.assertArrayEquals(expected, result);
-    }
 }
