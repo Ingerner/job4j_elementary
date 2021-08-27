30d700ddfa97cf67abc035087a16430311d31fa8
Oleg <korotkiy.o@yandex.ru>
2021-08-27 14:29:21 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -4,14 +4,13 @@ public class Defragment {
     public static String[] compress(String[] array) {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
-                for (int notNullIndex = index+1; notNullIndex < array.length; notNullIndex++) {
+                for (int notNullIndex = index + 1; notNullIndex < array.length; notNullIndex++) {
                     if (array[notNullIndex] != null) {
                         array[index] = array[notNullIndex];
                         array[notNullIndex] = null;
                         break;
                     }
                 }
-
             }
             System.out.print(array[index] + " ");
         }
--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -3,8 +3,8 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] world, char[] post) {
         boolean result = true;
-        for (int i = 0; i < post.length; i++) {
-            if (world[world.length - 1 - i] != post[post.length - 1 - i]) {
+        for (int index = 0; index < post.length; index++) {
+            if (world[world.length - 1 - index] != post[post.length - 1 - index]) {
                 result = false;
                 break;
             }
